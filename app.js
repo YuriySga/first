26229d67db0b982f25f67aaefc664c09544d9cad
YuriySga <ess1928@yandex.ru>
2020-04-09 13:36:07 +0400

    feat:1

---
+++ app.js
@@ -0,0 +1,13 @@
+let a = 5;
+let b = [1,2,3];
+
+array.forEach(function(item) {
+  console.log(item);
+});
+
+for (let index = 0; index < array.length; index++) {
+  const element = b[index];
+    
+}
+
+
